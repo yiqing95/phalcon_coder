aaed86303bc32e683844b38781392e33d3fe3678
yiqing <yiqing-95@qq.com>
2014-07-19 13:42:39 +0800

    Initial commit

---
+++ app/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yiqing
+ * Date: 14-7-19
+ * Time: 下午1:36
+ */
+
+class IndexController {
+    public function indexAction()
+    {
+        echo "<h1>Hello!</h1>";
+    }
+
+} 
---
+++ checker.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yiqing
+ * Date: 14-7-19
+ * Time: 上午2:08
+ */
+
+print_r(get_loaded_extensions());
